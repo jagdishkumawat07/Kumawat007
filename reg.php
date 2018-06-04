bec30ca206296c060f8c01c2672c7940dec7eb94
sunilkumwt <sunilkmwt10@gmail.com>
2018-06-04 15:22:06 +0530

    registration changes

--- reg.php
+++ reg.php
@@ -2,33 +2,96 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <div class="container">
-<h2 class="text-center">Welcome in 8Bit System Jaipur Vidhyadhar Nagar</h2>
+<h2 class="text-center">Welcome in Kumawat Samaj</h2>
 <div class="row">
 <div class="col-md-12">
 
 <form >
-<div class="form-group">
-<label>Email Address</label>
-<input type="text" name="t1"  class="form-control" placeholder="Enter the Email" />
+<div class="form-group; col-lg-6  " >
+ 
 <label>Name</label>
 <input type="text" name="t1"  class="form-control" placeholder="Enter the Name" />
+<label>UserName</label>
+<input type="text" name="t1"  class="form-control" placeholder="Enter the UserName" />
+<label>Email Address</label>
+<input type="text" name="t1"  class="form-control" placeholder="Enter the Email" />
+
+
+<label>Mobile no</label>
+<input type="text" name="t3" class="form-control" placeholder="Enter the mobile no"/>
+<label>Age</label>
+<select name="age" class="form-control" >
+<option value="0"> Select Your age </option>
+<option value="18">18</option><option value="19">19</option><option value="20" >20</option><option value="21">21</option><option value="22">22</option><option value="23">23</option><option value="24">24</option><option value="25">25</option><option value="26">26</option><option value="27">27</option><option value="28">28</option><option value="29">29</option><option value="30">30</option><option value="31">31</option><option value="32">32</option><option value="33">33</option><option value="34">34</option><option value="35">35</option><option value="36">36</option><option value="37">37</option><option value="38">38</option><option value="39">39</option><option value="40">40</option><option value="41">41</option><option value="42">42</option><option value="43">43</option><option value="44">44</option><option value="45">45</option>      
+              
+   
+  </select>
+
+<label>Gender</label>
+<div class="form-control">
+  <input type="radio" name="gender" value="male"   /> Male 
+  <input type="radio" name="gender" value="female" /> Female
+  </div>
 <label>Address</label>
 <textarea name="address" class="form-control" placeholder="Enter the Address" ></textarea>
 <label>City</label>
-<select name="r" class="form-control">
-<option>Jaipur</option>
-<option>Jaipur</option>
-<option>Jaipur</option>
-<option>Jaipur</option>
+<input type="text" name="t4" class="form-control" placeholder="Enter the city Name" />
+<label>State</label>
+<select name="stName"  class="form-control " >
+    <option value="0">---Select State Name---</option>
+    <option value="AS">Assam</option>
+    <option value="BR">Bihar</option>
+    <option value="CH">Chandigarh</option>
+    <option value="CG">Chhattisgarh</option>
+    <option value="DL">Delhi</option>
+    <option value="GA">Goa</option>
+    <option value="GJ">Gujarat</option>
+    <option value="HR">Haryana</option>
+    <option value="HP">Himachal Pradesh</option>
+    <option value="JK">Jammu and Kashmir</option>
+    <option value="JH">Jharkhand</option>
+    <option value="KA">Karnataka</option>
+    <option value="KL">Kerala</option>
+    <option value="MH">Maharashtra</option>
+    <option value="MN">Manipur</option>
+    <option value="ML">Meghalaya</option>
+    <option value="OD">Odisha</option>
+    <option value="PY">Pondicherry</option>
+    <option value="PB">Punjab</option>
+    <option value="RJ">Rajasthan</option>
+    <option value="SK">Sikkim</option>
+    <option value="TN">Tamil Nadu</option>
+    <option value="TR">Tripura</option>
+    <option value="UK">Uttarakhand</option>
+    <option value="UP">Uttar Pradesh</option>
+    <option value="WB">West Bengal</option>
+
 
 </select>
-<label>Hobbies</label>
-<input type="text" name="t1"  class="form-control" placeholder="Enter the Email" />
+<label> Marital Status </label>
+<select class="form-control" id="sel1">
+<option value="never">Never married</option>
+    <option value="single">Married in past, now single</option>
+   
+  </select>
+  
+  
+  <label> Educaton</label>
+ <input type="text" name="t7"  class="form-control" placeholder="Enter the Education" />
 
+<label>Hobbies</label>
+<div class="form-control">
+<label class="checkbox-inline" ><input type="checkbox" name="Hobbies" value="football"  > Football</label>
+ <label class="checkbox-inline"><input type="checkbox" name="Hobbies" value="cricket" >Cricket</label>
+ </div>
+ 
+ <br>
+ <br>
+<button type="submit" class="btn btn-success" name="t" >Submit</button>
+<button type="reset" class="btn btn-danger" name="t" >Reset</button>
 
 </div>
-<button type="submit" class="btn btn-success" name="t" >Save<i class="glyphicon glyphicon-search"></i></button>
-<button type="reset" class="btn btn-danger" name="t" >Reset</button>
+
 </form><br />
 <br />
 <br />

cece88d1eaa20228382d7ded471327d231ec4613
unknown <jagdishkumawat07@gmail.com>
2018-06-04 13:15:30 +0530

    kumawat samaj code

--- reg.php
+++ reg.php
@@ -2,7 +2,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <div class="container">
-<h2 class="text-center">Welcome in 8Bit System Jaipur</h2>
+<h2 class="text-center">Welcome in 8Bit System Jaipur Vidhyadhar Nagar</h2>
 <div class="row">
 <div class="col-md-12">
 
